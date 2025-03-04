454a4d8fba153c243b02b6e4a03dd08e28de1eee
Wamique <wamik.abro212@gmail.com>
2025-03-05 02:56:16 +0500

    hello world to python once again :)

---
+++ HelloWorld.py
@@ -0,0 +1,15 @@
+# This program ssays hello n asks name
+print('Hello, world!')
+
+print('What is your name?')
+name = input()
+
+print('It is good to meet you, ' + name)
+
+print('The length of your name is:')
+print(len(name))
+
+print('What is your age?')
+age = input()
+
+print('You will be ' + str(int(age) + 1)  + ' in a year.')
