9b5ca7840dd3b8db90ee668a2e74f93e37e03712
Wamique <wamik.abro212@gmail.com>
2025-03-05 04:06:06 +0500

    round off

---
+++ RoundingOff.py
@@ -0,0 +1,8 @@
+number = 1.4453; # a float number
+print(number)
+
+number2 = round(number) # round off the float number
+print(number2)
+
+number3 = round(number, 2) # round off the float number to 2 decimal places
+print(number3)
