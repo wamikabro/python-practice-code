ec71bff6f0a4a5662928f59be8aa813dad55c21c
Wamique <wamik.abro212@gmail.com>
2025-03-17 20:49:15 +0500

    use enumerate to iterate list in for loop

---
+++ enumerateFunction.py
@@ -0,0 +1,7 @@
+supplies = ['pens', 'staplers', 'flamethrowers', 'binders']
+
+# for index in range(len(supplies)):
+#    print('Item in ' + str(index) + ' is ' + supplies[index])
+
+for index, item in enumerate(supplies):
+    print('Item is ' + str(index) + ' is ' + item)
