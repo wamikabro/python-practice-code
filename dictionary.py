2921aba3e4d29bb5ec900d504ace8615bde23dd8
Wamique <wamik.abro212@gmail.com>
2025-03-18 20:30:40 +0500

    dictionary demo

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+spam = ['cats', 'dogs', 'moose']
+bacon = ['dogs', 'cats', 'moose']
+
+print(spam==bacon) # False
+
+eggs = {'name': 'Sophia', 'species':'cat', 'age': '8'}
+ham = {'species': 'cat', 'age': '8', 'name': 'Sophia'}
+
+print(eggs==ham)
