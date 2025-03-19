66155d6b062579bb57c595b20afd093f19920ddf
Wamique <wamik.abro212@gmail.com>
2025-03-19 22:31:21 +0500

    multilline strings

---
+++ catnapping.py
@@ -0,0 +1,16 @@
+""" This is 
+multiline
+comment """
+
+''' This 
+is 
+also a 
+multiline comment'''
+
+
+print('''Dear Alice,
+
+Eve's cat has been arrested for catnapping, cat burglary, and extortion.
+
+Sincerely,
+Wamik''')
