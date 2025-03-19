f6bd3b2d3cdd759c4df94a98a27dde71524ca3e7
Wamique <wamik.abro212@gmail.com>
2025-03-19 22:35:20 +0500

    different types of strings

---
+++ stringInterpolationAndFormatString.py
@@ -0,0 +1,8 @@
+name = 'Wamique'
+age = 21
+
+print('Hello, my name is ' + name + '. I am ' + str(age) + ' years old.')
+
+print('My name is %s. I am %s years old.' % (name, age))
+
+print(f'My name is {name}. Next year I will be {age+1}.')
