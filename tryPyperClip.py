381b8aadbc781e3b3a479f76f333154ffebf0854
Wamique <wamik.abro212@gmail.com>
2025-03-20 21:24:19 +0500

    pyperclip intruption

--- tryPyperClip.py
+++ tryPyperClip.py
@@ -6,7 +6,9 @@ pyperclip.copy(name)
 
 #
 #
-#
-#
+# use input to stop execution and try copying anyting
+# then give the input and the paste command will paste the
+# thing that you had copied from somewhere else
+# input()
 
 print(pyperclip.paste())
