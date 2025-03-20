5aef9fd665addfb6b7b9b7ffbb16d71b41bd8f15
Wamique <wamik.abro212@gmail.com>
2025-03-20 16:57:13 +0500

    join vs split

---
+++ splitVsJoin.py
@@ -0,0 +1,9 @@
+name = 'hello honey'
+
+splittedName = name.split(' ')
+print(splittedName) # ['hello', 'honey']
+
+aList = ['item 1', 'item 2', 'item 3']
+
+joinedWord = ' '.join(aList)
+print(joinedWord) # item 1, item 2, item 3
