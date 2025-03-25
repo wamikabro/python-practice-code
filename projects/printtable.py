51396b11eb3c89382639c338a123ac725bbecec1
Wamique <wamik.abro212@gmail.com>
2025-03-25 09:13:06 +0500

    print table in a spacific way using rjust

--- projects/printtable.py
+++ projects/printtable.py
@@ -5,6 +5,8 @@ tableData = [['apples', 'oranges', 'cherries', 'banana'],
 def printTable(tableData):
     # list to store max width of each column
     colWidths = [0] * len(tableData) # [0,0,0]
+    # if we didn't use [] with zero, python wouldn't have 
+    # created the list, and kept 000 as integer which is just 0
 
     # find longest string in each column and add in colWidths
     for i in range(len(tableData)): # 0, 1, 2
@@ -12,13 +14,25 @@ def printTable(tableData):
     #   colWidths 1 = 5
     #   colWidths 2 = 5
         colWidths[i] = max(len(word) for word in tableData[i])
-        
+    
+    for i in range(len(tableData[0])):
+        for j in range(len(tableData)):
+            print(tableData[j][i].rjust(colWidths[j]), end='  ')
+        print('\n')
+
+
+    #for index, value in enumerate(tableData):
+     #   for word in tableData[index]: #[0,1,2]
+      #      print(word.rjust(colWidths[index]))
+            
+
+            
 
 
 
 
 
-
+printTable(tableData)
 
 
 
