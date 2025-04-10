08a1abc0a44208bed467f2bfb51586bdb2d3bd68
Wamique <wamik.abro212@gmail.com>
2025-04-10 19:33:38 +0500

    walk directory usuing os module

---
+++ walkDirectoryTree.py
@@ -0,0 +1,13 @@
+import os
+from pathlib import Path
+
+for folderName, subfolders, filenames in os.walk(Path.cwd()):
+    print('The current folder is ' + folderName)
+
+    for subfolder in subfolders:
+        print('SUBFOLDER OF ' + folderName + ': ' + subfolder)
+
+    for filename in filenames:
+        print('FILE INSIDE ' + folderName + ': '+ filename)
+
+    print('')
