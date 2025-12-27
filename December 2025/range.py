410b7ba26ed43fc661cbbc4ccce9ad0f7957e0e1
Wamique <wamik.abro212@gmail.com>
2025-12-27 19:01:11 +0500

    fundamentals of range as an iterator

---
+++ December 2025/range.py
@@ -0,0 +1,17 @@
+# range() function returns and iterator
+# 
+for i in range(5):
+    print(i)
+
+# line break
+print()
+
+# specifying start and end and addition
+# even numbers between 2 to 10
+# start is included but end is excluded
+for i in range(2, 10, 2):
+    print(i)
+
+
+# converting range to list 
+print(list(range(5)))
