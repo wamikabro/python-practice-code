d7e6928ad5cb55c644c468caffa0b73a0da094a7
Wamique <wamik.abro212@gmail.com>
2025-12-28 21:39:59 +0500

    in and not in operators

---
+++ December 2025/InAndNotIn.py
@@ -0,0 +1,11 @@
+# list
+my_list = [1, 2, 3, 4, 5]
+print("Original list:", my_list)
+
+# 'in' operator
+if 3 in my_list:
+    print("3 is in the list")
+
+# 'not in' operator
+if 6 not in my_list:
+    print("6 is not in the list")
